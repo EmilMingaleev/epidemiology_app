9b3322912a4859c6f902a1e56c54ac66660b3684
EmilMingaleev <mingaleev_emil@mail.ru>
2025-04-01 17:57:43 +0300

    rqrcode

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_01_123520) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_01_135002) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -74,6 +74,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_01_123520) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "qr_code"
   end
 
   create_table "reports", force: :cascade do |t|
