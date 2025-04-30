bca7b1c0aa510cf106d8eeb37d05bf8f721261a7
EmilMingaleev <mingaleev_emil@mail.ru>
2025-04-30 16:32:22 +0300

    enrypoint db

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,9 @@
 set -e
 
 rm -f tmp/pids/server.pid
-#
-echo "Resetting database..."
-bin/rails db:drop DISABLE_DATABASE_ENVIRONMENT_CHECK=1
-bin/rails db:create
-bin/rails db:migrate
 
-echo "Checking if seeding is needed..."
+bin/rails db:prepare
+
 if bin/rails runner "exit User.any? ? 0 : 1"; then
   echo "Skipping seeds (already seeded)"
 else
