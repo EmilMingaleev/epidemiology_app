23a899c264ca88fa4e41f426cc4373dfc45978f4
EmilMingaleev <mingaleev_emil@mail.ru>
2025-04-30 17:18:25 +0300

    123

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,10 @@ set -e
 # Удаляем PID-файл Puma, если он остался после перезапуска
 rm -f tmp/pids/server.pid
 
-# Выполняем миграции
-bin/rails db:prepare
+echo "Resetting database..."
+bin/rails db:drop DISABLE_DATABASE_ENVIRONMENT_CHECK=1
+bin/rails db:create
+bin/rails db:migrate
 
 # Прогоняем сиды, только если база пуста (например, нет пользователей)
 if bin/rails runner "exit User.any? ? 0 : 1"; then
