6aacff797886cdb4f15fbda73fd14949dd5f6b7d
EmilMingaleev <mingaleev_emil@mail.ru>
2025-05-05 19:05:16 +0300

    service api

--- app/services/epidemiology_news_service.rb
+++ app/services/epidemiology_news_service.rb
@@ -6,27 +6,30 @@ require 'cgi'
 class EpidemiologyNewsService
   API_KEY = '0cdb1b3635cf4784a4ce82018ed1056a'
 
-  def self.fetch_epidemiology_news
-    query = 'эпидемиология'
+  def self.fetch_news(query)
     encoded_query = CGI.escape(query)
     url = URI("https://newsapi.org/v2/everything?q=#{encoded_query}&language=ru&apiKey=#{API_KEY}")
     response = Net::HTTP.get(url)
     JSON.parse(response)['articles'].first(3)
   end
 
-  def self.fetch_hiv_news
-    query = 'ВИЧ'
-    encoded_query = CGI.escape(query)
-    url = URI("https://newsapi.org/v2/everything?q=#{encoded_query}&language=ru&apiKey=#{API_KEY}")
-    response = Net::HTTP.get(url)
-    JSON.parse(response)['articles'].first(3)
-  end
+  def self.fetch_all_news
+    queries = {
+      epidemiology: 'эпидемиология',
+      hiv: 'ВИЧ',
+      covid: 'COVID'
+    }
 
-  def self.fetch_covid_news
-    query = 'COVID'
-    encoded_query = CGI.escape(query)
-    url = URI("https://newsapi.org/v2/everything?q=#{encoded_query}&language=ru&apiKey=#{API_KEY}")
-    response = Net::HTTP.get(url)
-    JSON.parse(response)['articles'].first(3)
+    results = {}
+    threads = []
+
+    queries.each do |key, query|
+      threads << Thread.new do
+        results[key] = fetch_news(query)
+      end
+    end
+
+    threads.each(&:join)
+    results
   end
 end
